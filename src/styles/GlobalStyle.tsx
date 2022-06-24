ead4ea2f93ee87d171306316fd099ad0362ea884
João Batista <jbsouzasantos1999@gmail.com>
2022-06-23 23:24:00 -0300

    Setting up the environment

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Roboto Mono", monospace;
+  color: #000000;
+  scroll-behavior: smooth;
+  transition: ease-in-out;
+  
+  &::selection {
+    background-color: #ffffff;
+    color: #000000;
+  }
+}
+
+button {
+    cursor: pointer;
+    border: none;    
+}
+
+ul {
+    list-style: none;
+}
+
+a {
+  text-decoration: none;  
+}
+
+h2 {
+  font-size: 22px;
+}`;
